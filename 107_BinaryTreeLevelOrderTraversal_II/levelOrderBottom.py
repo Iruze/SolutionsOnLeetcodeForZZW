9dd04ee320ece1a44a1038c64d9fc897dff5b845
Iruze <42053722+Iruze@users.noreply.github.com>
2019-11-06 12:30:47 +0800

    Update levelOrderBottom.py

--- 107_BinaryTreeLevelOrderTraversal_II/levelOrderBottom.py
+++ 107_BinaryTreeLevelOrderTraversal_II/levelOrderBottom.py
@@ -1,1 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+from collections import deque
+
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        output = []
+        cur_nodes = deque()
+        cur_nodes.append(root)
+        while cur_nodes:
+            nex_nodes = deque()
+            cur_val = []
+            for nd in cur_nodes:
+                if nd:
+                    cur_val.append(nd.val)
+                    nex_nodes.extend([nd.left, nd.right])
+            if cur_val:
+                output.insert(0, cur_val)
+            cur_nodes = nex_nodes
+        return output
