25b7a6fff7d48498b6b531a391202f2470dea9b5
Iruze <42053722+Iruze@users.noreply.github.com>
2019-11-28 22:56:24 +0800

    Create partition.py

---
+++ 86_PartitionList/partition.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        small, large = ListNode(-1), ListNode(-1)
+        small_head, large_head = small, large
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                large.next = head
+                large = large.next
+            head = head.next
+        small.next, large.next = large_head.next, None
+        return small_head.next
