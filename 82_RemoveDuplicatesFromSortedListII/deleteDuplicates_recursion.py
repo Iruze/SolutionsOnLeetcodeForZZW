f1588cbbc47f4c19c21376050bd4c95dc92ef2b9
Iruze <42053722+Iruze@users.noreply.github.com>
2020-01-12 15:39:44 +0800

    Create deleteDuplicates_recursion.py

---
+++ 82_RemoveDuplicatesFromSortedListII/deleteDuplicates_recursion.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head: return None
+        if head.next and head.val == head.next.val:
+            while head.next and head.val == head.next.val:
+                head = head.next
+            return self.deleteDuplicates(head.next)
+        else:
+            head.next = self.deleteDuplicates(head.next)
+        return head
