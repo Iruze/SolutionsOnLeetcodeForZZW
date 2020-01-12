42c7ba47c444a2ed937bef988f91f0c5bd87583d
Iruze <42053722+Iruze@users.noreply.github.com>
2020-01-12 20:06:10 +0800

    Create Foo_threadingLock.py

---
+++ 1114_PrintInOrder/Foo_threadingLock.py
@@ -0,0 +1,40 @@
+import threading
+
+class Foo(object):
+    def __init__(self):
+        self.l1 = threading.Lock()
+        self.l1.acquire()
+        self.l2 = threading.Lock()
+        self.l2.acquire()
+
+    def first(self, printFirst):
+        """
+        :type printFirst: method
+        :rtype: void
+        """
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.l1.release()
+
+
+    def second(self, printSecond):
+        """
+        :type printSecond: method
+        :rtype: void
+        """
+        
+        # printSecond() outputs "second". Do not change or remove this line.
+        self.l1.acquire()
+        printSecond()
+        self.l2.release()
+            
+    def third(self, printThird):
+        """
+        :type printThird: method
+        :rtype: void
+        """
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        self.l2.acquire()
+        printThird()
