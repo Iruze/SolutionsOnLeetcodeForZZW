66ae1d976eb2e04366dc41e5b260ee3b6d804e5d
Iruze <42053722+Iruze@users.noreply.github.com>
2020-01-18 16:09:34 +0800

    Update test.sql

--- 596_ClassesMoreThan5Students/test.sql
+++ 596_ClassesMoreThan5Students/test.sql
@@ -7,3 +7,11 @@ HAVING COUNT(DISTINCT c.student) >= 5;
 
 
 # solutions2:
+SELECT class
+FROM
+    (
+        SELECT class, COUNT(DISTINCT student) num
+        FROM courses
+        GROUP BY class
+    ) temp_talbe
+WHERE num >= 5;
