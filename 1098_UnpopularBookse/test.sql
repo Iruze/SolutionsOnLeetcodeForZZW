b52813cfab95b968804cb0f15b616102cbf401ca
Iruze <42053722+Iruze@users.noreply.github.com>
2020-01-19 18:07:17 +0800

    Update test.sql

--- 1098_UnpopularBookse/test.sql
+++ 1098_UnpopularBookse/test.sql
@@ -9,12 +9,11 @@ HAVING IFNULL(SUM(IF(dispatch_date >= DATE_ADD('2019-06-23',INTERVAL -1 YEAR), q
 
 
 # solution2:
-# Write your MySQL query statement below
-SELECT books.book_id,books.name
-FROM books
-LEFT OUTER JOIN orders
-ON books.book_id=orders.book_id 
-AND orders.dispatch_date >= DATE_ADD('2019-06-23',INTERVAL -1 YEAR)
-WHERE books.available_from <= DATE_ADD('2019-06-23',INTERVAL -1 MONTH)
-GROUP BY book_id
-HAVING IFNULL(SUM(quantity),0)<10;
+SELECT b.book_id, b.name
+FROM Books b
+LEFT OUTER JOIN Orders o
+ON b.book_id = o.book_id
+AND o.dispatch_date >= DATE_ADD('2019-06-23', INTERVAL -1 YEAR)
+WHERE b.available_from <= DATE_ADD('2019-06-23', INTERVAL -1 MONTH)
+GROUP BY b.book_id
+HAVING IFNULL(SUM(o.quantity), 0) < 10
