185528927cd99b9c27bcd7d165d5a2dd606d0dbc
Iruze <42053722+Iruze@users.noreply.github.com>
2020-01-19 17:41:08 +0800

    Update test.sql

--- 1098_UnpopularBookse/test.sql
+++ 1098_UnpopularBookse/test.sql
@@ -4,7 +4,7 @@ FROM books
 LEFT OUTER JOIN orders
 ON books.book_id=orders.book_id 
 WHERE books.available_from <= DATE_ADD('2019-06-23',INTERVAL -1 MONTH)
-GROUP BY books.book_id
+GROUP BY book_id
 HAVING IFNULL(SUM(IF(dispatch_date >= DATE_ADD('2019-06-23',INTERVAL -1 YEAR), quantity, 0)),0)<10;
 
 
@@ -16,5 +16,5 @@ LEFT OUTER JOIN orders
 ON books.book_id=orders.book_id 
 AND orders.dispatch_date >= DATE_ADD('2019-06-23',INTERVAL -1 YEAR)
 WHERE books.available_from <= DATE_ADD('2019-06-23',INTERVAL -1 MONTH)
-GROUP BY books.book_id
+GROUP BY book_id
 HAVING IFNULL(SUM(quantity),0)<10;
