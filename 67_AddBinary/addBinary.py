751d54925d5c88809be36182c35268afccd42c0c
Iruze <42053722+Iruze@users.noreply.github.com>
2020-01-30 15:04:35 +0800

    Update addBinary.py

--- 67_AddBinary/addBinary.py
+++ 67_AddBinary/addBinary.py
@@ -3,11 +3,14 @@ class Solution:
         i, j = len(a) - 1, len(b) - 1
         res = ''
         carry = 0
+        # 从尾到头对位相加
         while i >= 0 or j >= 0:
             n1 = int(a[i]) if i >= 0 else 0
             n2 = int(b[j]) if j >= 0 else 0
             tmp = n1 + n2 + carry
+            # 二进制进位
             carry = tmp // 2
             res = str(tmp % 2) + res
             i, j = i - 1, j - 1
+        # 最高位如果有进位，需要加上carry
         return res if not carry else '1' + res
