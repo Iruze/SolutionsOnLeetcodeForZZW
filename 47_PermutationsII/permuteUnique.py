f8fd72a7abd278801a6c78f5ee8a0e5b0bbaea51
Iruze <42053722+Iruze@users.noreply.github.com>
2020-02-22 15:21:45 +0800

    Update permuteUnique.py

--- 47_PermutationsII/permuteUnique.py
+++ 47_PermutationsII/permuteUnique.py
@@ -1,4 +1,4 @@
-# 解法一：
+# 解法一：递归-回溯：交换nums
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         def _backtrack(first=0):
@@ -14,4 +14,26 @@ class Solution:
         return output
 
 
-# 解法二：
+# 解法二：递归-回溯：visited记录访问足迹
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def _dfs(depth=0, cur=[]):
+            if depth == len(nums):
+                output.append(cur[:])
+            for i in range(len(nums)):
+                if not visited[i]:
+                    if 0 < i and nums[i - 1] == nums[i] and not visited[i - 1]:
+                        continue
+                    visited[i] = True
+                    cur.append(nums[i])
+                    _dfs(depth + 1, cur)
+                    cur.pop()
+                    visited[i] = False
+        if not nums: 
+            return []
+        nums.sort()
+        output = []
+        visited = [False for _ in range(len(nums))]
+        _dfs()
+        return output
+        
