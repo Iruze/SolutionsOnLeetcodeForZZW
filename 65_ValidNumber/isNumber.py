02e8768189d8aa039787af29d766666bcc68fe5c
Iruze <42053722+Iruze@users.noreply.github.com>
2020-02-23 21:12:54 +0800

    Create isNumber.py

---
+++ 65_ValidNumber/isNumber.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        if not s: return False
+        sl = re.split(r'[eE]', s.strip())
+        # e前后不包含数字、s中含有多个e或E
+        if '' in sl or len(sl) > 2:
+            return False
+        # e之后又空格
+        if sl[0][-1] == ' ':
+            return False
+        try:
+            float(sl[0])
+        except:
+            return False
+        if len(sl) == 2:
+            # e之前有空格
+            if sl[1][0] == ' ':
+                return False
+            try:
+                int(sl[1])
+            except:
+                return False
+        return True 
