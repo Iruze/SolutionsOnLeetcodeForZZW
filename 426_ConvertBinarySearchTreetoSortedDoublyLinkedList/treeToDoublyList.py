dba549e762f1e8f70d2ba7aa00607e08e377f6dc
Iruze <42053722+Iruze@users.noreply.github.com>
2020-03-01 11:07:25 +0800

    Update treeToDoublyList.py

--- 426_ConvertBinarySearchTreetoSortedDoublyLinkedList/treeToDoublyList.py
+++ 426_ConvertBinarySearchTreetoSortedDoublyLinkedList/treeToDoublyList.py
@@ -31,3 +31,26 @@ class Solution:
         pLastNodeInList = pCurrNode
         if pLastNodeInList.right:
             self.__convertNode(pLastNodeInList.right)
+
+# 写法二：
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        def _convert(root):
+            if not root:
+                return
+            _convert(root.left)
+            root.left = self.last
+            if self.last:
+                self.last.right = root
+            self.last = root
+            _convert(root.right)
+        # 调用_convert()函数
+        if not root:
+            return
+        self.last = None
+        _convert(root)
+        head = self.last
+        while head and head.left:
+            head = head.left
+        head.left, self.last.right = self.last, head
+        return head
