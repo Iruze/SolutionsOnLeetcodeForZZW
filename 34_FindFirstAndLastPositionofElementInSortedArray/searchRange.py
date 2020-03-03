4e599bc82191215328cd3d57e5fccf924b28a395
Iruze <42053722+Iruze@users.noreply.github.com>
2020-03-03 11:52:25 +0800

    Update searchRange.py

--- 34_FindFirstAndLastPositionofElementInSortedArray/searchRange.py
+++ 34_FindFirstAndLastPositionofElementInSortedArray/searchRange.py
@@ -14,3 +14,26 @@ class Solution:
         return [left, right - 1]
 
 # 解法二：标准的二分查找
+# 实质是用_posLR()函数替代bisect.bisect_left()和bisect.bisect_right()函数
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums: return [-1, -1]
+        left = self._posLR(nums, target, True)
+        if left == len(nums) or nums[left] != target:
+            return [-1, -1]
+        right = self._posLR(nums, target, False)
+        return [left, right - 1]
+    
+    def _posLR(self, nums, target, isLeft):
+        # 注意： right = len(nums)
+        left, right = 0, len(nums)
+        while left < right:
+            mid = left + ((right - left) >> 1)
+            # 1. target < nums[mid]
+            # 2. 左寻找，且target == nums[mid]，此时当做target < nums[mid]，
+            #    这样才能向left左边靠拢
+            if target < nums[mid] or isLeft and target == nums[mid]:
+                right = mid
+            else:
+                left = mid + 1
+        return left
