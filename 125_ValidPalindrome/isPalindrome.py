3424d92fb93287294c1621cda97494f5f25e4395
Iruze <42053722+Iruze@users.noreply.github.com>
2020-03-09 19:22:03 +0800

    Update isPalindrome.py

--- 125_ValidPalindrome/isPalindrome.py
+++ 125_ValidPalindrome/isPalindrome.py
@@ -1,7 +1,8 @@
+# 解法一：双指针解法
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         def isValid(s):
-            return '0' <= s and s <= '9' or 'a' <= s and s <= 'z' or 'A' <= s and s <= 'Z'
+            return '0' <= s <= '9' or 'a' <= s <= 'z' or 'A' <= s <= 'Z'
         left, right = 0, len(s) - 1
         while left < right:
             if not isValid(s[left]):
@@ -14,3 +15,10 @@ class Solution:
                 return False
             left, right = left + 1, right - 1
         return True
+
+
+# 解法二： 运用filter函数
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = [*filter(str.isalnum, s.lower())]
+        return s == s[::-1]
