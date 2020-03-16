61690a3921529e5fc6d01b147897686e422ea5a4
Iruze <42053722+Iruze@users.noreply.github.com>
2020-03-16 12:31:29 +0800

    Update maxAreaOfIsland_bfs.py

--- 695_MaxAreaOfIsland/maxAreaOfIsland_bfs.py
+++ 695_MaxAreaOfIsland/maxAreaOfIsland_bfs.py
@@ -3,30 +3,30 @@ class Solution:
         if not grid or not grid[0]: return 0
         rows, cols = len(grid), len(grid[0])
         visited = [[False for _ in range(cols)] for _ in range(rows)]
+        max_area, cur_area = 0, 0
         deque = collections.deque()
-        max_area = 0
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1 and not visited[r][c]:
+                if not visited[r][c] and grid[r][c] == 1:
                     visited[r][c] = True
                     deque.append((r, c))
-                    cur_area = self.__bfs(rows, cols, grid, visited, deque)
+                    cur_area = self.__bfs(grid, visited, deque)
                     max_area = max(max_area, cur_area)
         return max_area
     
-    def __bfs(self, rows, cols, grid, visited, deque):
+    def __bfs(self, grid, visited, deque):
         cur_area = 0
         while deque:
             r, c = deque.popleft()
             cur_area += 1
-            for nr, nc in self.__neighbour(r, c, grid, visited):
+            for nr, nc in self.__neighbour(grid, visited, r, c):
                 visited[nr][nc] = True
                 deque.append((nr, nc))
         return cur_area
 
-    def __neighbour(self, r, c, grid, visited):
+    
+    def __neighbour(self, grid, visited, r, c):
         rows, cols = len(grid), len(grid[0])
         for nr, nc in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
-            if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1 and not visited[nr][nc]:
+            if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc] and grid[nr][nc] == 1:
                 yield nr, nc
-        
