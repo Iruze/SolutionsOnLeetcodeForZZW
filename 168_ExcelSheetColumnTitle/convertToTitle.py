32e15577096a7eb3844e6e55e731469719f8f7f4
Iruze <42053722+Iruze@users.noreply.github.com>
2020-03-17 11:38:59 +0800

    Update convertToTitle.py

--- 168_ExcelSheetColumnTitle/convertToTitle.py
+++ 168_ExcelSheetColumnTitle/convertToTitle.py
@@ -1,3 +1,4 @@
+# 方法一：字典，递归
 class Solution:
     def convertToTitle(self, n: int) -> str:
         # 映射表 0-25 映射字母 ‘A’ - ‘Z’
@@ -5,3 +6,9 @@ class Solution:
         # 递归解法
         if n <= 26: return table[n - 1]
         return self.convertToTitle((n - 1) // 26) + table[(n - 1) % 26]
+
+
+# 方法二： 一行递归
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        return "" if n == 0 else self.convertToTitle((n - 1) // 26) + chr((n - 1) % 26 + 65)
