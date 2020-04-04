c4663e10e6dfae9b8d26213a7c7ea2cc441d2677
Iruze <42053722+Iruze@users.noreply.github.com>
2020-04-04 17:42:30 +0800

    Update maxProfit.py

--- 309_BestTimetoBuyandSellStockwithCooldown/maxProfit.py
+++ 309_BestTimetoBuyandSellStockwithCooldown/maxProfit.py
@@ -4,12 +4,13 @@ class Solution:
         n = len(prices)
         dp = [[0, 0] for i in range(n)]
         for i in range(n):
-            if i == 0:
+            # 因为冷冻期，base case 分需单独考虑头两天的交易情况
+            if i == 0:                  # 第一天的 base case
                 dp[i][0] = 0
                 dp[i][1] = -prices[0]
                 continue
             dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
-            if i == 1:
+            if i == 1:                  # 第二天的 base case
                 dp[i][1] = max(dp[0][1], -prices[i])
                 continue
             dp[i][1] = max(dp[i - 1][1], dp[i - 2][0] - prices[i])
