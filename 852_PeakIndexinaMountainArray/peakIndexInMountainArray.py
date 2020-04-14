79ccbb3146a0ea87ce63ff6d3e066278dd3283a6
Iruze <42053722+Iruze@users.noreply.github.com>
2020-04-14 18:18:10 +0800

    Create peakIndexInMountainArray.py

---
+++ 852_PeakIndexinaMountainArray/peakIndexInMountainArray.py
@@ -0,0 +1,6 @@
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        for i in range(1, len(A) - 1):
+            if A[i - 1] < A[i] and A[i] > A[i + 1]:
+                return i
+        return -1
