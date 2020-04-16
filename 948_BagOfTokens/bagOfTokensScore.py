77c9e147e79edd43475e36a597ada43cb6c14c54
Iruze <42053722+Iruze@users.noreply.github.com>
2020-04-16 20:44:02 +0800

    Update bagOfTokensScore.py

--- 948_BagOfTokens/bagOfTokensScore.py
+++ 948_BagOfTokens/bagOfTokensScore.py
@@ -1,3 +1,11 @@
+"""
+先对数组排序，使用双指针，left，right。
+根据贪心策略，
+令牌正面朝上，我们需要尽可能失去少的能量，从左往右，left++
+令牌反面朝上，我们需要尽可能多得到能量，从右往左，right--
+每次维护最大的得分即可。
+"""
+
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], P: int) -> int:
         lo, hi = 0, len(tokens) - 1
