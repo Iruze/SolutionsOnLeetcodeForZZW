46396394294e2abe0681085bea85f54cf2d4f932
Iruze <42053722+Iruze@users.noreply.github.com>
2020-04-22 12:38:37 +0800

    Update maxWidthRamp.py

--- 962_MaximumWidthRamp/maxWidthRamp.py
+++ 962_MaximumWidthRamp/maxWidthRamp.py
@@ -8,6 +8,7 @@ class Solution:
                 stack.append(i)
         
         for i in range(len(A) - 1, -1, -1):
+            # 一定要带上=，在=时要弹出值
             while stack and A[i] >= A[stack[-1]]:
                 ans = max(ans, i - stack.pop())
         return ans
