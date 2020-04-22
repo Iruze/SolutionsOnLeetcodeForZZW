b553520f16526a3de6d850497a730aa6c23baae8
Iruze <42053722+Iruze@users.noreply.github.com>
2020-04-22 12:36:57 +0800

    Create maxWidthRamp.py

---
+++ 962_MaximumWidthRamp/maxWidthRamp.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxWidthRamp(self, A: List[int]) -> int:
+        stack = []
+        ans = 0
+        # 最大宽度的左端必然在stack中
+        for i, v in enumerate(A):
+            if not stack or A[stack[-1]] > A[i]:
+                stack.append(i)
+        
+        for i in range(len(A) - 1, -1, -1):
+            while stack and A[i] >= A[stack[-1]]:
+                ans = max(ans, i - stack.pop())
+        return ans
