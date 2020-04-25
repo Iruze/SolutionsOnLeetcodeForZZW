b7283afb47300c53fe4a57fcc4339470d297a4b7
Iruze <42053722+Iruze@users.noreply.github.com>
2020-04-25 20:18:03 +0800

    Create rand10.py

---
+++ 470_ImplementRand10UsingRand7/rand10.py
@@ -0,0 +1,16 @@
+# 拒接采样算法
+
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+class Solution:
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        num = (rand7() - 1) * 7 + rand7()
+        # num > 30, 20, 10都行，不过概率低，时间效率差
+        while num > 40:
+            num = (rand7() - 1) * 7 + rand7()
+        return (num - 1) % 10 + 1
