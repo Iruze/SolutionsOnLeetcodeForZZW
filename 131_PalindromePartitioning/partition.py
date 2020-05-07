0fcc459b656743730c71d392920e22e7e00f6deb
Iruze <42053722+Iruze@users.noreply.github.com>
2020-05-07 22:15:50 +0800

    Update partition.py

--- 131_PalindromePartitioning/partition.py
+++ 131_PalindromePartitioning/partition.py
@@ -1,19 +1,15 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res = []
-        if s:
-            self.__dfs(s, res, [])
-        return res
-    
+        ans = []
 
-    def __dfs(self, s, res, pre=[]):
-        if s == s[::-1]:
-            res.append(pre[:] + [s])
-            if len(s) <= 1: return
-        for i in range(1, len(s)):
-            d, s = s[:i], s[i:]
-            if d == d[::-1]:
-                pre.append(d)
-                self.__dfs(s, res, pre)
-                pre.pop()
-            s = d + s
+        def helper(s, tmp):
+            if not s:
+                ans.append(tmp)
+            # 分割出来的第一个回文串的长度，最长为len(s)
+            # 这样递归的时候s中剩下的就为空，触发base case
+            for i in range(1, len(s) + 1):
+                if s[:i] == s[:i][::-1]:
+                    helper(s[i:], tmp + [s[:i]])
+        
+        helper(s, [])
+        return ans
