bc90f4fa7c177bd5cd7f977ed6c868ffca3b0268
Iruze <42053722+Iruze@users.noreply.github.com>
2020-10-17 18:55:33 +0800

    Update permuteUnique.py

--- 47_PermutationsII/permuteUnique.py
+++ 47_PermutationsII/permuteUnique.py
@@ -37,3 +37,19 @@ class Solution:
         _dfs()
         return output
         
+# 解法三: 递归-回溯: visited为一个数, 将空间复杂度从O(n)降到O(1)
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(nums, visited=0, cur=[], depth=0):
+            if depth == len(nums):
+                ans.append(cur[::])
+                return
+            for i, num in enumerate(nums):
+                if visited & (1<<i) == 0:
+                    if i > 0 and nums[i] == nums[i - 1] and visited & (1<<(i - 1)) == 0:
+                        continue
+                    backtrack(nums, visited | (1 << i), cur + [nums[i]], depth + 1)
+        nums.sort()
+        ans = []
+        backtrack(nums)
+        return ans
