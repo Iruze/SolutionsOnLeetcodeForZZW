8ae81a8e22c2ad2ff640d101db620f59bc125cee
Iruze <42053722+Iruze@users.noreply.github.com>
2020-11-19 16:15:02 +0800

    Update search.py

--- 33_SearchinRotatedSortedArray/search.py
+++ 33_SearchinRotatedSortedArray/search.py
@@ -27,3 +27,26 @@ class Solution:
             return idx
         return -1
 ```
+
+
+# 解法二：
+```python
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = lo + ((hi - lo) >> 1)
+            if nums[mid] == target:
+                return mid
+            if nums[lo] <= nums[mid]:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            else:
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+        return -1
+```
