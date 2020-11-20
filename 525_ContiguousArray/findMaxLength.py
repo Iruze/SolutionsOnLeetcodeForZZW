0946bb10a19fbe2dd5e537cec5b9342d6936fa29
Iruze <42053722+Iruze@users.noreply.github.com>
2020-11-20 20:05:19 +0800

    Update findMaxLength.py

--- 525_ContiguousArray/findMaxLength.py
+++ 525_ContiguousArray/findMaxLength.py
@@ -6,6 +6,7 @@ class Solution:
         # 将0，1数目相等转化为求和为k=0的最长连续子数组
         # 思路同 #325. 和等于k的最长子数组长度
         for i, num in enumerate(nums):
+            # 建模为： 0则-1， 1则+1， ”区间和“为0的时候则0和1数量相等
             SUM += 2 * num - 1
             if SUM in lookup:
                 ans = max(ans, i - lookup[SUM])
