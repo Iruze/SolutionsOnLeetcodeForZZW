e50d8c219cf695c5090fb36677be735826ae7c55
Iruze <42053722+Iruze@users.noreply.github.com>
2020-11-20 20:03:54 +0800

    Update findMaxLength.py

--- 525_ContiguousArray/findMaxLength.py
+++ 525_ContiguousArray/findMaxLength.py
@@ -1,18 +1,14 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         lookup = {0 : -1}
-        res = 0
+        ans = 0
+        SUM = 0
         # 将0，1数目相等转化为求和为k=0的最长连续子数组
         # 思路同 #325. 和等于k的最长子数组长度
-        for idx, val in enumerate(nums):
-            if val == 0:
-                nums[idx] = -1
-        cur = 0
-        for idx, val in enumerate(nums):
-            # 记录前缀和
-            cur += val
-            if cur in lookup:
-                res = max(res, idx - lookup[cur])
+        for i, num in enumerate(nums):
+            SUM += 2 * num - 1
+            if SUM in lookup:
+                ans = max(ans, i - lookup[SUM])
             else:
-                lookup[cur] = idx
-        return res
+                lookup[SUM] = i
+        return ans     
