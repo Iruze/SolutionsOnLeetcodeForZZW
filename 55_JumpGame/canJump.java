725b6555be5ec751f8f1c1094e4b95a269424a93
Iruze <42053722+Iruze@users.noreply.github.com>
2020-11-26 12:54:06 +0800

    Update canJump.java

--- 55_JumpGame/canJump.java
+++ 55_JumpGame/canJump.java
@@ -1,28 +1,35 @@
-# Solution2: 动态规划（自底向上）
-
-enum Idx {
-    BAD, UNKNOWN, GOOD
-}
-
+//  自底向上
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        
-        Idx[] dp = new Idx[n];
-        for(int i = 0; i < n; ++i)
-            dp[i] = Idx.UNKNOWN;
-        dp[n - 1] = Idx.GOOD;
-        
-        for(int i = n - 2; i >= 0; --i) {
-            int furthestIdx = Math.min(n - 1, nums[i] + i);
-            for(int j = i + 1; j <= furthestIdx; ++j) {
-                if(dp[j] == Idx.GOOD) {
-                    dp[i] = Idx.GOOD;
-                    break;
-                }
+        int start = nums.length - 2, end = nums.length - 1;
+        while(start >= 0) {
+            if(start + nums[start] >= end) {
+                end = start;
+            }
+
+            --start;
+        }
+
+        return end <= 0;
+    }
+}
+
+
+
+// 自顶向下
+class Solution {
+    public boolean canJump(int[] nums) {
+        int end = 0;
+        for(int i = 0; i < nums.length; ++i) {
+            if(end < i) {
+                return false;
+            }
+            end = Math.max(end, i + nums[i]);
+            if(end >= nums.length - 1) {
+                return true;
             }
         }
-        
-        return dp[0] == Idx.GOOD;
+
+        return false; 
     }
 }
