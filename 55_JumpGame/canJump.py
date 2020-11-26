b98c4182bca6a7618a1a19b30752e784105e05d2
Iruze <42053722+Iruze@users.noreply.github.com>
2020-11-26 13:17:56 +0800

    Update canJump.py

--- 55_JumpGame/canJump.py
+++ 55_JumpGame/canJump.py
@@ -1,36 +1,19 @@
-# Solution1: 贪心算法
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        lastBingo = len(nums) - 1
-        for i in range(len(nums) - 1, -1, -1):
-            if nums[i] + i >= lastBingo:
-                lastBingo = i
-        return lastBingo == 0
-        
-# Solution2: 动态规划（自底向上）
-enum Idx {
-    BAD, UNKNOWN, GOOD
-}
+        # # 自顶向下
+        # end = 0
+        # for idx in range(len(nums)):
+        #     if end < idx:
+        #         return False
+        #     end = max(end, idx + nums[idx])
+        #     if end >= len(nums) - 1:
+        #         return True
+        # return False
 
-class Solution {
-    public boolean canJump(int[] nums) {
-        int n = nums.length;
-        
-        Idx[] dp = new Idx[n];
-        for(int i = 0; i < n; ++i)
-            dp[i] = Idx.UNKNOWN;
-        dp[n - 1] = Idx.GOOD;
-        
-        for(int i = n - 2; i >= 0; --i) {
-            int furthestIdx = Math.min(n - 1, nums[i] + i);
-            for(int j = i + 1; j <= furthestIdx; ++j) {
-                if(dp[j] == Idx.GOOD) {
-                    dp[i] = Idx.GOOD;
-                    break;
-                }
-            }
-        }
-        
-        return dp[0] == Idx.GOOD;
-    }
-}
+        # 自底向上
+        start, end = len(nums) - 2, len(nums) - 1
+        while start >= 0:
+            if start + nums[start] >= end:
+                end = start
+            start -= 1
+        return end <= 0
