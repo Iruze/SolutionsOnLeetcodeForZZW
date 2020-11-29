c5099091d5aa7dd6f07b79e75e905abd7c43cddc
Iruze <42053722+Iruze@users.noreply.github.com>
2020-11-29 16:44:26 +0800

    Update minDistance.py

--- 72_EditDistance/minDistance.py
+++ 72_EditDistance/minDistance.py
@@ -1,9 +1,7 @@
-import numpy
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        if not word1 and not word2: return 0
         m, n = len(word1), len(word2)
-        dp = numpy.zeros((m + 1, n + 1))
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
         for i in range(1, m + 1):
             dp[i][0] = i
         for j in range(1, n + 1):
@@ -13,5 +11,8 @@ class Solution:
                 if word1[i - 1] == word2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
                 else:
-                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
-        return int(dp[m][n])
+                    # dp[i - 1][j - 1] 替代word1[i]个字符
+                    # dp[i - 1][j] 删除: 删掉word1[i]字符
+                    # dp[i][j - 1] 插入: 在word1中重新插入字符word[j]
+                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1
+        return dp[-1][-1]
