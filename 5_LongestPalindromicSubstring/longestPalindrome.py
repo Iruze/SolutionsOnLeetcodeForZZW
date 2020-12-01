ee83fd759533ed4cbae5db4e2f1acda8ebaeeca6
Iruze <42053722+Iruze@users.noreply.github.com>
2020-12-01 22:43:50 +0800

    Update longestPalindrome.py

--- 5_LongestPalindromicSubstring/longestPalindrome.py
+++ 5_LongestPalindromicSubstring/longestPalindrome.py
@@ -1,19 +1,33 @@
+# 方法一： 马拉车算法
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         s = '#' + '#'.join(s) + '#'
-        RL = [0] * len(s)
-        maxRight, pos = 0, 0
-        res = ''
+        rl = [0 for _ in range(len(s))]
+        pos, max_right = 0, 0
+        ans = ''
         for i in range(len(s)):
-            if i < maxRight:
-                RL[i] = min(RL[2 * pos - i], maxRight - i)
+            if i < max_right:
+                rl[i] = min(rl[2 * pos - i], max_right - i)
             else:
-                RL[i] = 1
-            while i - RL[i] >= 0 and i + RL[i] < len(s) and s[i - RL[i]] == s[i + RL[i]]:
-                RL[i] += 1
-            if RL[i] - 1 + i > maxRight:
-                maxRight = RL[i] - 1 + i
+                rl[i] = 1
+            while 0 <= i - rl[i] and i + rl[i] < len(s) and s[i - rl[i]] == s[i + rl[i]]:
+                rl[i] += 1
+            if i + rl[i] - 1 > max_right:
+                max_right = i + rl[i] - 1
                 pos = i
-            if RL[i] - 1 > len(res):
-                res = s[i-RL[i]+1:i+RL[i]].replace('#', '')
-        return res
+            ans = max(ans, s[i-rl[i]+1:i+rl[i]], ans, key=len)
+        return ans.replace('#', '')
+
+# 方法二： dp
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        dp = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = 1
+            for j in range(i - 1, -1, -1):
+                if s[i] == s[j]:
+                    dp[j][i] = dp[j + 1][i - 1] + 2
+                else:
+                    dp[j][i] = max(dp[j][i - 1], dp[j + 1][i])
+        return dp[0][n - 1]
