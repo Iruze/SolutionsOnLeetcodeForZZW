82921d0555de941c415316aad5f3e6421c00b6e9
Iruze <42053722+Iruze@users.noreply.github.com>
2020-12-03 00:29:40 +0800

    Update isScramble.py

--- 87_ScrambleString/isScramble.py
+++ 87_ScrambleString/isScramble.py
@@ -1,11 +1,15 @@
-# 区间dp
 class Solution:
     def isScramble(self, s1: str, s2: str) -> bool:
-        if s1 == s2:
-            return True
-        if Counter(s1) != Counter(s2):
+        
+        @functools.lru_cache(None)
+        def dfs(s1, s2):
+            if s1 == s2: return True
+            if Counter(s1) != Counter(s2): return False
+            for i in range(1, len(s1)):
+                syn = dfs(s1[:i], s2[:i]) and dfs(s1[i:], s2[i:])
+                mir = dfs(s1[:i], s2[-i:]) and dfs(s1[i:], s2[:-i])
+                if syn or mir:
+                    return True
             return False
-        for i in range(1, len(s1)):
-            if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]) or self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-i]):
-                return True
-        return False
+        
+        return dfs(s1, s2)
