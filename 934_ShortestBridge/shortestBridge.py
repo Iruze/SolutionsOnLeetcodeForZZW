01c2f321501453d9d9207190de772e5dc6bb4ab5
Iruze <42053722+Iruze@users.noreply.github.com>
2020-12-05 18:05:25 +0800

    Update shortestBridge.py

--- 934_ShortestBridge/shortestBridge.py
+++ 934_ShortestBridge/shortestBridge.py
@@ -1,32 +1,39 @@
 """
-dfs + bfs解法：
-dfs将第一个岛的1全部入队，bfs扩散搜索入队的第一个岛的1到第二个岛的1的距离。
+染色法, 一次DFS + 一次BFS
+1). "染色法"先将小岛1的所有位置标记为2, 并送入队列;
+2). 所有队列中的所有位置, 向周围"扩散", 即继续"染色"
+3). 当触及到小岛2(A[nr][nc] == 1), 立即返回结果
 """
+
 class Solution:
     def shortestBridge(self, A: List[List[int]]) -> int:
         rows, cols = len(A), len(A[0])
-        d = collections.deque()
+        island1 = collections.deque()
         collected = False
-        for r in range(rows):                   # dfs搜索，将第一个岛中1全部入队
+        # 将小岛1的所有位置加入到队列island1中
+        for r in range(rows):
             if any(A[r]):
                 c = A[r].index(1)
-                self._dfs(r, c, A, d)
+                # dfs深度遍历搜索所有小岛1的位置
+                self._dfs(r, c, A, island1)
                 break
-        
-        while d:                                # bfs扩散，找到到达1的距离
-            r, c = d.popleft()
+        while island1:
+            r, c = island1.popleft()
             for nr, nc in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
                 if 0 <= nr < rows and 0 <= nc < cols:
-                    if A[nr][nc] == 1:          # 找到了，它就是第二个岛
-                        return A[r][c] - 2      # 第一个岛本身值为1， 为了标记已经访问，加了1以区分，所以结果要减去2
+                    # 触及到了小岛2, 返回结果
+                    if A[nr][nc] == 1:
+                        return A[r][c] - 2
+                    # 只触及到了岛之间的水,继续'染色'
                     elif A[nr][nc] == 0:
                         A[nr][nc] = A[r][c] + 1
-                        d.append((nr, nc))
+                        island1.append((nr, nc))
         return A[r][c] - 2
     
-    def _dfs(self, r, c, A, d):
+    def _dfs(self, r, c, A, island1):
+        # 染色法: 将本来的'1'染成'2', 省去了传统的visited空间
         A[r][c] += 1
-        d.append((r, c))
+        island1.append((r, c))
         for nr, nc in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
             if 0 <= nr < len(A) and 0 <= nc < len(A[0]) and A[nr][nc] == 1:
-                self._dfs(nr, nc, A, d)
+                self._dfs(nr, nc, A, island1)
