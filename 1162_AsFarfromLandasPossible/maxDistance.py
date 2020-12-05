a27c50f665d1eb1604566b5a02e1077435d239df
Iruze <42053722+Iruze@users.noreply.github.com>
2020-12-05 20:55:57 +0800

    Update maxDistance.py

--- 1162_AsFarfromLandasPossible/maxDistance.py
+++ 1162_AsFarfromLandasPossible/maxDistance.py
@@ -1,19 +1,26 @@
 class Solution:
-    def maxDistance(self, grid: List[List[int]]) -> int:
-        if not grid or not grid[0]:
-            return -1
+    def closedIsland(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        d = collections.deque()
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    d.append((r, c))
-        ans = -1
-        while d:
-            r, c = d.popleft()
-            for nr, nc in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
-                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 0:
-                    grid[nr][nc] = grid[r][c] + 1
-                    ans = max(ans, grid[nr][nc])
-                    d.append((nr, nc))
-        return ans - 1 if ans > 0 else -1
+        cnt = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 0:
+                    # 如果以(i, j)开始的岛屿,是一条封闭的,结果+1
+                    cnt += self._isLand(grid, i, j)
+        return cnt
+    
+    def _isLand(self, grid, i, j):
+        # 默认当前岛屿是"封闭的"
+        bingo = True
+        # 将当前岛屿位置置为2, 避免重复访问, 省下visited的 m*n 空间
+        grid[i][j] = 2
+        rows, cols = len(grid), len(grid[0])
+        for ni, nj in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
+            # 在边界的1显然不是封闭的岛屿
+            if not (0 <= ni < rows and 0 <= nj < cols):
+                bingo = False
+            # 邻接0,继续dfs递归搜索
+            elif grid[ni][nj] == 0:
+                # 利用 & "短路"性质
+                bingo = self._isLand(grid, ni, nj) and bingo
+        return bingo
