7b524cc3d044668c01f12fb92e610cdb42ea0fcd
Iruze <42053722+Iruze@users.noreply.github.com>
2020-12-13 17:59:17 +0800

    Update smallestStringWithSwaps.py

--- 1202_SmallestStringWithSwaps/smallestStringWithSwaps.py
+++ 1202_SmallestStringWithSwaps/smallestStringWithSwaps.py
@@ -22,3 +22,51 @@ class Solution:
             for i, c in zip(sorted(seq), words):
                 ans[i] = c
         return ''.join(ans)
+
+    
+    
+    
+    
+    
+# 并查集的完整解法
+class Solution:
+    
+    def __init__(self):
+        self.parent = dict()
+        self.size = dict()
+
+    def initialize(self, n):
+        self.parent = {i: i for i in range(n)}
+        self.size = {i: 1 for i in range(n)}
+
+    def find(self, x):
+        while x != self.parent[x]:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+    
+    def union(self, p, q):
+        rootp, rootq = self.find(p), self.find(q)
+        if rootp == rootq:
+            return
+        if self.size[rootp] > self.size[rootq]:
+            self.parent[rootq] = rootp
+            self.size[rootp] += self.size[rootq]
+        else:
+            self.parent[rootp] = rootq
+            self.size[rootq] += self.size[rootp]
+
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        n = len(s)
+        self.initialize(n)
+        for p, q in pairs:
+            self.union(p, q)
+        d = collections.defaultdict(list)
+        for i, val in enumerate(map(self.find, self.parent)):
+            d[val].append(i)
+        ans = ['' for _ in range(n)]
+        for seq in d.values():
+            words = sorted(s[idx] for idx in seq)
+            for i, c in zip(seq, words):
+                ans[i] = c
+        return ''.join(ans)
