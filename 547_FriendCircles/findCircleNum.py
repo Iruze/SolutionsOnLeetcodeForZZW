a279e6cdb93090b6b61a9733594decc01297e6ba
Iruze <42053722+Iruze@users.noreply.github.com>
2020-12-13 14:08:35 +0800

    Update findCircleNum.py

--- 547_FriendCircles/findCircleNum.py
+++ 547_FriendCircles/findCircleNum.py
@@ -1,37 +1,43 @@
 class Solution:
-    def __init__(self):
-        self.tree_num = 0
-        self.tree_node = []
-        self.parent = []
-    
-    def initialize_tree(self, n):
-        self.tree_num = n 
-        self.tree_node = [1] * n 
-        self.parent = [i for i in range(n)]
 
+    def __init__(self):
+        self.parent = dict()
+        self.size = dict()
+        self.count = 0
+
+    def initialize(self, n):
+        self.parent = {i: i for i in range(n)}
+        self.size = {i : 1 for i in range(n)}
+        self.count = n
+    
+    def find(self, x):
+        while x != self.parent[x]:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+    
     def union(self, p, q):
         rootp = self.find(p)
         rootq = self.find(q)
         if rootp == rootq:
             return
-        if self.tree_node[p] > self.tree_node[q]:
-            self.parent[rootp] = rootp
-            self.tree_node[rootp] += self.tree_node[rootq]
+        # 路径压缩，size记录两颗树大小
+        # 小的树接到大的树上，保持新树平衡
+        if self.size[rootq] < self.size[rootp]:
+            self.parent[rootq] = rootp
+            self.size[rootp] += self.size[rootq]
         else:
             self.parent[rootp] = rootq
-            self.tree_node[rootq] += self.tree_node[rootp]
-        self.tree_num -= 1
-    
-    def find(self, x):
-        while self.parent[x] != x:
-            self.parent[x] = self.parent[self.parent[x]]
-            x = self.parent[x]
-        return x
-    
+            self.size[rootq] += self.size[rootp]
+        # 两颗树连接后，树的总数减1
+        self.count -= 1
+
     def findCircleNum(self, M: List[List[int]]) -> int:
-        self.initialize_tree(len(M))
-        for i in range(len(M)):
-            for j in range(i):
+        n = len(M)
+        self.initialize(n)
+        for i in range(n):
+            # 只需要遍历对角线（不含对角线）右边的一半
+            for j in range(i + 1, n):
                 if M[i][j] == 1:
                     self.union(i, j)
-        return self.tree_num
+        return self.count
