3ca3ceba08856ec2f1b8ebd67db3bffbae408b16
Iruze <42053722+Iruze@users.noreply.github.com>
2021-01-04 23:58:45 +0800

    Update exist.py

--- 79_WordSearch/exist.py
+++ 79_WordSearch/exist.py
@@ -30,3 +30,35 @@ class Solution(object):
             for j in range(len(board[0])):
                 if hasPathCore(i, j, 0): return True
         return False
+
+# python, dfs解法
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        def dfs(i, j, word, visited=set()):
+            # Base case
+            if not word:
+                return True
+            for ni, nj in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
+                # 搜索相邻的，且没有被访问过的位置
+                if 0 <= ni < m and 0 <= nj < n and (ni, nj) not in visited:
+                    # 这个位置字符和word开头对上了
+                    if board[ni][nj] == word[0]:
+                        # 在下一层中，找到了一个成功的方向，即刻返回true
+                        if dfs(ni, nj, word[1:], visited | {(ni, nj)}):
+                            return True
+            return False
+
+        m, n = len(board), len(board[0])
+        for i in range(m):
+            for j in range(n):
+                # 开头对上了，进入下一层寻找
+                if board[i][j] == word[0]:
+                    # 剩下的依然匹配，则返回true
+                    if dfs(i, j, word[1:], set([(i, j)])):
+                        return True
+        return False
