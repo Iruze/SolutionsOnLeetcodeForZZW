825bed1b4738be6aee8bad6ac2e3205a130140af
Iruze <42053722+Iruze@users.noreply.github.com>
2021-01-09 16:15:29 +0800

    Update numDecodings.py

--- 91_DecodeWays/numDecodings.py
+++ 91_DecodeWays/numDecodings.py
@@ -1,15 +1,12 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s or s[0] == '0':                                   # 01xxx 无效
-            return 0
-        f1, f2, f, n = 1, 1, 1, len(s)
-        for i in range(1, n):                                      # 从1开始，len(s)<2，直接返回 f=1
-            if s[i] == '0':                                        # 当前位是否为‘0’分类讨论
-                if s[i - 1] == '1' or s[i - 1] == '2':             # 110x, 120x 时：f = f1
-                    f = f1
-                else:                                              # 100x, 130x 无效
-                    return 0
-            elif s[i - 1] == '1' or s[i - 1] == '2' and s[i] <= '6':
-                    f = f1 + f2                                    # 非边界，如：1212，1122
-            f1, f2 = f2, f                                         # 下一次迭代设置
-        return f
+        
+        @functools.lru_cache(None)
+        def helper(s):
+            if not s: return 1
+            if s[0] == '0': return 0
+            if len(s) > 1 and s[:2] <= '26':
+                return helper(s[1:]) + helper(s[2:])
+            return helper(s[1:])
+        
+        return helper(s)
