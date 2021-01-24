51d76aec7a4fafadd477c1c73a83c9d149c06af9
Iruze <42053722+Iruze@users.noreply.github.com>
2021-01-24 16:13:41 +0800

    Update calcEquation.py

--- 399_EvaluateDivision/calcEquation.py
+++ 399_EvaluateDivision/calcEquation.py
@@ -3,32 +3,34 @@ python3, 图+dfs 解法
 """
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        from collections import defaultdict
-        graph = defaultdict(dict)
+        graph = collections.defaultdict(dict)
         var = set()
-        ans = []
         for item, value in zip(equations, values):
             var.update(item)
             s, e = item[0], item[1]
             graph[s][e] = value
             graph[e][s] = 1.0 / value
-        
-        def dfs(s, e, visited):
+
+        @functools.lru_cache(None)
+        def dfs(s, e):
             if s == e:
                 return 1.0
             visited.add(s)
-            rnt = 1.0
+            ret = 1.0
             for nxt in graph[s]:
                 if nxt not in visited:
-                    rnt = graph[s][nxt] * dfs(nxt, e, visited)
-                    if rnt > 0:
-                        return rnt
-            return -1
+                    ret = graph[s][nxt] * dfs(nxt, e)
+                    if ret > 0:
+                        return ret
+            visited.remove(s)
+            return -1.0
         
+        ans = list()
+        visited = set()
         for s, e in queries:
             if s not in var or e not in var:
                 ans.append(-1.0)
             else:
-                ans.append(dfs(s, e, set()))
-        
+                visited.clear()
+                ans.append(dfs(s, e))
         return ans
