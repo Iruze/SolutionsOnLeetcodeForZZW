3b97d8322a3377f0a052827509c0da9738c09b8e
Iruze <42053722+Iruze@users.noreply.github.com>
2021-02-19 13:28:13 +0800

    Update numDecodings.py

--- 91_DecodeWays/numDecodings.py
+++ 91_DecodeWays/numDecodings.py
@@ -1,3 +1,4 @@
+# 方法一: 记忆化递归
 class Solution:
     def numDecodings(self, s: str) -> int:
         
@@ -10,3 +11,20 @@ class Solution:
             return helper(s[1:])
         
         return helper(s)
+    
+# 方法二: 青蛙跳台阶解法
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0] == '0':
+            return 0
+        f1, f2, f = 1, 1, 1
+        for i in range(1, len(s)):
+            if s[i] == '0':
+                if s[i - 1] in {'1', '2'}:
+                    f = f1
+                else:
+                    return 0
+            elif s[i - 1] == '1' or s[i - 1] == '2' and s[i] <= '6':
+                f = f1 + f2
+            f1, f2 = f2, f
+        return f
