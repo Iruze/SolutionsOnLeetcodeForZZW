4f5a1e2bfff1239e4ac6d657742775daff1d08cd
Iruze <42053722+Iruze@users.noreply.github.com>
2021-02-28 16:23:06 +0800

    Update wordBreak.py

--- 139_WordBreak/wordBreak.py
+++ 139_WordBreak/wordBreak.py
@@ -1,17 +1,27 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        if not wordDict:
-            return False
-        if not s:
-            return True
+        # list转化为set, 加快查询
+        wordSet = set(wordDict)
         n = len(s)
-        dp = [False for _ in range(n + 1)]
-        dp[0] = True
-        # dp里面的dp[i]表示基1的第i个元素
-        for i in range(1, n + 1):
-            for j in range(i):
-                # s[j:i]是基0的，相当于基1的第j+1个元素开始
-                if dp[j] and s[j:i] in wordDict:
-                    dp[i] = True
-                    break 
-        return dp[-1]
+        # # 解法一: 标准的dp
+        # dp = [False] * (n + 1)
+        # dp[0] = True
+        # for i in range(1, n + 1):
+        #     for j in range(i):
+        #         if dp[j] and s[j:i] in wordSet:
+        #             dp[i] = True
+        #             break
+        # return dp[-1]
+
+        # 解法二: dp
+
+        @functools.lru_cache(None)
+        def dfs(s):
+            if not s: return True
+            included = False
+            for i in range(1, n + 1):
+                if s[:i] in wordSet:
+                    included |= dfs(s[i:])
+            return included
+        
+        return dfs(s)
