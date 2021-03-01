046cfe5c7f5445da2d8daddb1924bdb3129b7946
Iruze <42053722+Iruze@users.noreply.github.com>
2021-03-01 19:06:28 +0800

    Update countDigitOne.py

--- 233_NumberOfDigitOne/countDigitOne.py
+++ 233_NumberOfDigitOne/countDigitOne.py
@@ -1,53 +1,32 @@
-# 解法一： c++ 版本
-
-#include <cmath>
-#include <string>
-
-using namespace std;
-
-class Solution {
- public:
-  int countDigitOne(int n) {
-    if (n <= 0) { return 0; }
-    if (n < 10) { return 1; }
-
-    string strnum = to_string(n);
-
-    int highNum = strnum[0] - '0';
-    int size = strnum.size();
-    int withoutHigh = n - highNum * pow(10, size - 1);
-
-    // 1. 统计首位为1出现次数:
-    //    - 若n首位是1(如12345), 那么有 2346个
-    //    - 若n的首位U不是1(如23456), 那么有 10000个
-    int firstOneCount = highNum == 1 ? (withoutHigh + 1)
-                        : pow(10, size-1);
-    // 统计其他位为1的出现次数
-    // 2. 大于 3456
-    //    - 可以划分为两个区间，例如对 23456 划分为
-    //      [10000, 19999], [3457, 9999]U[20001, 23456]
-    //    - 对任意一个区间，后面4个数字，选择其中一个为1，其他三个都可以在0~9中任意选择，因此一共有 2*4*10^3 个1
-    int otherOneCount = highNum * (size - 1) * pow(10, size - 2);
-
-    // 3. 递归求 [1, 3456]中1出现的次数
-    return firstOneCount + otherOneCount + countDigitOne(withoutHigh);
-  }
-};
-
-# 解法二： python3 版本
-
 class Solution:
     def countDigitOne(self, n: int) -> int:
-        if n <= 0:
+        """
+        思路: 以 5014 为例, 计算 "十位" 上的1的个数, 分为两部分:
+        hight = 501, low = 4
+        第一部分-高位:
+            [0-49]1[0-9]   一共 50 = hight // 10 *digit
+            考虑到如果是 5024的话, 实际高位是 
+            [0-50]1[0-9]    所以需要比较"十位"上的数和目标数target的关系, 如果 "十位" > target, 则[]高位总数+1], 否则[高位总数-1]
+            所以只需要加上修正因子 offset = 9 - target,
+            最终高位总数 = (hight + offset) // 10 * digit
+        
+        第二部分-低位:
+            计算完了高位[0-49]1[0-9], 低位就是 [50]1[0]-[50]1[4]了, 和明显是 4 + 1个, 但是如果 "十位" < target,
+            也就是如果是计算"2"的个数的话, 那么[50]1[0]-[50]1[4]就是0个了
+            所以, 
+            最终低位总数 = low + 1 if hight % 10 == target else 0 
+        """
+
+        if n == 0:
             return 0
-        strnum = str(n)
-        highnum = int(strnum[0])
-        size = len(strnum)
-        withouthigh = n - highnum * pow(10, size - 1)
-        if highnum > 1:
-            first_cnt = pow(10, size - 1)
-        elif highnum == 1:
-            first_cnt = withouthigh + 1
-        other_cnt = highnum * (size - 1) * pow(10, size - 2)
-        print(first_cnt, other_cnt, self.countDigitOne(withouthigh))
-        return int(first_cnt + other_cnt + self.countDigitOne(withouthigh))
+        if n <= 9:
+            return 1
+        digit = 1
+        cnt = 0
+        target = 1
+        offset = 9 - target
+        while digit <= n:
+            hight, low = n // digit, n % digit
+            cnt += (hight + offset) // 10 * digit + (low + 1 if hight % 10 == target else 0)
+            digit *= 10
+        return cnt
