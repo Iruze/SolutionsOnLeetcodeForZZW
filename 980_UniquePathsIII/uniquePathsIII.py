2d22a06ade38deee676d504c52d961a746388183
Iruze <42053722+Iruze@users.noreply.github.com>
2021-03-21 16:29:04 +0800

    Update uniquePathsIII.py

--- 980_UniquePathsIII/uniquePathsIII.py
+++ 980_UniquePathsIII/uniquePathsIII.py
@@ -29,3 +29,37 @@ class Solution:
         visited[start[0]][start[1]] = True
         dfs(start[0], start[1], rest)
         return self.ans
+
+    
+# 方法二: 记忆化递归, 在上面解法基础之上, 存储已经走过的路径结果
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+
+        pos = lambda r, c: 1 << (r * cols + c)
+
+        rows, cols = len(grid), len(grid[0])
+        travel_set = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] % 2 == 0:
+                    travel_set |= pos(r, c)
+                if grid[r][c] == 1:
+                    sr, sc = r, c
+        
+        def neighbour(r, c):
+            for nr, nc in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
+                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] % 2 == 0:
+                    yield nr, nc
+
+        @functools.lru_cache(None)
+        def dfs(r, c, travel):
+            if grid[r][c] == 2:
+                return +(travel == 0)
+            path_cnt = 0
+            for nr, nc in neighbour(r, c):
+                if pos(nr, nc) & travel:
+                    path_cnt += dfs(nr, nc, pos(nr, nc) ^ travel)
+            return path_cnt
+
+        return dfs(sr, sc, travel_set)
+        
