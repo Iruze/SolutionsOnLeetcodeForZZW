bfe5d1105ade0b266a32ce79112ead95869cd300
Iruze <42053722+Iruze@users.noreply.github.com>
2021-03-24 21:51:26 +0800

    Update maxAreaOfIsland_bfs.py

--- 695_MaxAreaOfIsland/maxAreaOfIsland_bfs.py
+++ 695_MaxAreaOfIsland/maxAreaOfIsland_bfs.py
@@ -1,32 +1,30 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid or not grid[0]: return 0
         rows, cols = len(grid), len(grid[0])
-        visited = [[False for _ in range(cols)] for _ in range(rows)]
-        max_area, cur_area = 0, 0
+        max_area =0
         deque = collections.deque()
         for r in range(rows):
             for c in range(cols):
-                if not visited[r][c] and grid[r][c] == 1:
-                    visited[r][c] = True
+                if grid[r][c] == 1:
+                    # 直接修改原数组， 访问过了就标记为-1
+                    grid[r][c] = -1
                     deque.append((r, c))
-                    cur_area = self.__bfs(grid, visited, deque)
-                    max_area = max(max_area, cur_area)
+                    max_area = max(max_area, self.__bfs(grid, deque))
         return max_area
     
-    def __bfs(self, grid, visited, deque):
+    def __bfs(self, grid, deque):
         cur_area = 0
         while deque:
             r, c = deque.popleft()
             cur_area += 1
-            for nr, nc in self.__neighbour(grid, visited, r, c):
-                visited[nr][nc] = True
+            for nr, nc in self.__neighbour(grid, r, c):
                 deque.append((nr, nc))
         return cur_area
 
-    
-    def __neighbour(self, grid, visited, r, c):
+    def __neighbour(self, grid, r, c):
         rows, cols = len(grid), len(grid[0])
         for nr, nc in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
-            if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc] and grid[nr][nc] == 1:
+            if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                # 访问过了的(nr, nc)位置修改1为-1
+                grid[nr][nc] = -1
                 yield nr, nc
