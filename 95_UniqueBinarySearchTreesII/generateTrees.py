32ed8a61528353b96c4b57415b06b7f89cf4531a
Iruze <42053722+Iruze@users.noreply.github.com>
2021-03-26 10:57:30 +0800

    Update generateTrees.py

--- 95_UniqueBinarySearchTreesII/generateTrees.py
+++ 95_UniqueBinarySearchTreesII/generateTrees.py
@@ -1,23 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+# 记忆化递归
+
+
 
 class Solution:
     def generateTrees(self, n: int) -> List[TreeNode]:
-        if n <= 0: return []
+        
+        @functools.lru_cache(None)
+        # 注意: 这里是闭区间 [start, end]
         def helper(start, end):
-            res = []
-            if start > end: res.append(None)
+            tree = list()
+            if start > end:
+                tree.append(None)
             for val in range(start, end + 1):
-                for left in helper(start, val - 1):
-                    for right in helper(val + 1, end):
+                for lo in helper(start, val - 1):
+                    for hi in helper(val + 1, end):
                         root = TreeNode(val)
-                        root.left = left
-                        root.right = right
-                        res.append(root)
-            return res
-
+                        root.left, root.right = lo, hi
+                        tree.append(root)
+            return tree
+        
         return helper(1, n)
