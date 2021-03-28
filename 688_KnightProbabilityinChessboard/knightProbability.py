c10f353cc0b443fea1c7e9fc6cddc5f4b9199b07
Iruze <42053722+Iruze@users.noreply.github.com>
2021-03-28 20:12:54 +0800

    Update knightProbability.py

--- 688_KnightProbabilityinChessboard/knightProbability.py
+++ 688_KnightProbabilityinChessboard/knightProbability.py
@@ -1,11 +1,8 @@
-# 方法一：dfs + 备忘录
 class Solution:
-    def __init__(self):
-        self.d = dict()
-    
 
+    @lru_cache(None)
     def knightProbability(self, N: int, K: int, r: int, c: int) -> float:
-        def onceProbability(r, c):                                   # 搜索能够到达的棋盘上的位置
+        def onceProbability(r, c):
             board = [
                 (r + 2, c + 1),
                 (r + 2, c - 1),
@@ -19,14 +16,9 @@ class Solution:
             for nr, nc in board:
                 if 0 <= nr < N and 0 <= nc < N:
                     yield nr, nc
-        if K == 0:                                                    # DFS 的 base case
-            return 1
-        ans = 0
-        for nr, nc in onceProbability(r, c):
-            if (nr, nc, K) in self.d:
-                nxt_prob = self.d[(nr, nc, K)]                         # 如果备忘录中存在(nr, nc, K), 
-            else:                   
-                nxt_prob = self.knightProbability(N, K - 1, nr, nc)    # DFS返回下一个概率
-                self.d[(nr, nc, K)] = nxt_prob                         # 如果备忘录中不存在，则加入
-            ans += 0.125 * cur
-        return ans
+        
+        if K == 0: return 1
+        f = self.knightProbability
+        nbr = onceProbability(r, c)
+        # 如果不能跳到相邻的格子, 概率返回0
+        return sum(0.125 * f(N, K - 1, nr, nc) for nr, nc in nbr) if nbr else 0
