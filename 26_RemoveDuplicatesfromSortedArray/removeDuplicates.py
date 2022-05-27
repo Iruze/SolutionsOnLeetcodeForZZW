5f80155753e513ac5431761bf9c2a1aec953edf2
Iruze <42053722+Iruze@users.noreply.github.com>
2022-05-27 23:47:36 +0800

    Update removeDuplicates.py

--- 26_RemoveDuplicatesfromSortedArray/removeDuplicates.py
+++ 26_RemoveDuplicatesfromSortedArray/removeDuplicates.py
@@ -1,26 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        # 快慢指针解法，pre指向当前不重复元素
         pre = 0
-        for i in range(len(nums)):
-            if i - pre == 1:
-                if nums[i] != nums[pre]:
-                    pre = i
-                continue
-            elif i - pre > 1:
-                if nums[i] == nums[pre]:
-                    continue
-                else:
-                    pre += 1
-                    nums[pre] = nums[i]
-        return pre + 1 if nums else 0
-
-""" solution 2:
-class Solution:
-def removeDuplicates(self, nums: List[int]) -> int:
-    idx = 0
-    for num in nums:
-        if idx < 1 or nums[idx - 1] != num:
-            nums[idx] = num
-            idx += 1
-    return idx
-"""
+        for idx, num in enumerate(nums):
+            if idx > pre and nums[idx] != nums[pre]:
+                pre += 1
+                nums[pre] = nums[idx]
+        return pre + 1
